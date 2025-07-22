7c3edfedbf39eac05b42f71eb312455b627b6c85
Jan Thomas <scaria.jan.thomas@gmail.com>
2025-07-22 20:11:18 +0530

    3rd commit "Hosting"

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'my-portfolio' with your actual repo name if different
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [react()],
 })
